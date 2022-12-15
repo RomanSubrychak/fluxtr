98ab2396dbd8ed41538de2367404673563ad9c34
Roman Subrychak <rsubrichak@yahoo.com>
2022-12-14 21:54:59 -0600

    web server initial commit

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+#[get("/hello")]
+fn hello() -> &'static str {
+    "Hello"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index, hello])
 }
